5a20d316329152b4e77dc38df6f8b1acd03fbaa0
amsharidwan <ridwanamshari0310@gmail.com>
2026-05-14 16:34:40 +0700

    add: initializing flask app

---
+++ website/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'this is secret key'
+
+    from .views import views
+    from .auth import auth
+    from .admin import admin
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(admin, url_prefix='/')
+
+    return app
